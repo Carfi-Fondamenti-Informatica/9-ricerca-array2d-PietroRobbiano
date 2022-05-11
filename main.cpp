b208b38c92d42518587a606bbc5869a2520953e9
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-05-11 10:47:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    char nomi[10][20];
+    char nome[20];
+    init(nomi);
+    for ( int i=0; i<10; i++){
+        cin >> nomi[i];
+    }
+
+    init(nome);
+    cin>> nome;
+    int a = ricerca(nomi,nome);
+    cout << a << endl;
+    return 0;
 }
