f3dc4338a624a00d27aea05dac9b1a5ae9f8c48b
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-05-11 10:47:09 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,29 @@
+void init( char mat[20]){
+    for( int i=0; i<20; i++){
+        mat[i]= ' ';
+    }
+    return;
+}
 
+void init( char mat[10][20]){
+    for( int i=0; i<10; i++){
+        for( int j=0; j<20; j++){
+            mat[i][j]= ' ';
+        }
+    }
+    return;
+}
+
+int ricerca(char mat[10][20],char ar[20]){
+    for( int i=0; i<10; i++){
+        for( int j=0; j<20; j++){
+            if( mat[i][j] != ar[j]){
+                break;
+            } else if (mat[i][j] == ar[j]){
+                if (j==19){
+                    return i;
+                }
+            }
+        }
+    }
+}
