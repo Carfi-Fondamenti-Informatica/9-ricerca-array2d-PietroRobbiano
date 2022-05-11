3c443581905f189699e1ceeeb6f7c97ae545f5fd
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-05-11 10:46:44 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+#ifndef ESERCIZIO_9_LIB_H
+#define ESERCIZIO_9_LIB_H
 
+#endif //ESERCIZIO_9_LIB_H
+
+void init( char mat[20]);
+void init( char mat[10][20]);
+int ricerca(char mat[10][20],char ar[20]);
